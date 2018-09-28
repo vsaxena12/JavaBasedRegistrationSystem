c94c50570bfe0e312d390ecec10151d620082261
Varun Saxena <36344027+vsaxena12@users.noreply.github.com>
2018-09-28 19:56:58 -0400

    Add files via upload

---
+++ src/wwic/WWIC.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package wwic;
+
+/**
+ *
+ * @author hp
+ */
+public class WWIC {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
